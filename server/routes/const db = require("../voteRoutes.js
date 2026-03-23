37516b62f5c5ec42f3f0e117c9194620286e3da9
Babul Kumar <199328506+BabulKumar100@users.noreply.github.com>
2026-03-23 23:47:07 +0530

    Removed tracked files and reapplied gitignore

--- server/routes/const db = require("../voteRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const voteController = require("../controllers/voteController");
-
-router.post("/vote", voteController.voteResource);
-
-module.exports = router;
