455e38269b268222088c10aeec0a21715092bed4
Pawan Singh <pawan9140582015@gmail.com>
2026-03-29 10:57:48 +0530

    Create theme-provider.jsx

---
+++ client/src/components/theme-provider.jsx
@@ -0,0 +1,10 @@
+'use client'
+
+import * 'react'
+import {
+  ThemeProvider,
+} from 'next-themes'
+
+export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+}
